e6add0f8ff2e81507f4b559e1cece1c172d0a1b4
Anna Lepak <anna.lepak@adesso.de>
2023-01-30 16:22:15 +0100

    created webpage with slider and blocks

---
+++ dom/slider_blocks/scripts.js
@@ -0,0 +1,30 @@
+let sliderValEl = document.getElementById('sliderValue');
+let slider = document.getElementById('boxSlider');
+const boxWrapper = document.querySelector('.box__container');
+const button = document.querySelector('.button');
+
+sliderValEl.innerHTML = slider.value;
+
+button.addEventListener('click', (e) => {
+  button.classList.toggle("change");
+});
+
+slider.addEventListener('change', (e) => {
+  sliderValEl.innerHTML = e.target.value;
+  document.querySelectorAll(".box").forEach(el => el.remove());
+  for(let i = 0; i < e.target.value; i++) {
+    let box = document.createElement('div');
+    box.classList.add('box');
+    box.style.background = random_bg_color();
+    boxWrapper.appendChild(box);
+  }
+});
+
+function random_bg_color() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor;
+}
+
